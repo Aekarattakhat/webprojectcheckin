1be8617d8957364f0c381a7bebb71ec59276be88
=Aekarattakhat <=aekarattakhat.s@kkumail.com>
2025-02-21 17:47:31 +0700

    add init file

---
+++ src/components/modal/qandA.js
@@ -0,0 +1,2 @@
+//หน้าจอถามตอบ
+//เป็น modal เอาไปใส่ใน classroom mange compoent
---
+++ src/components/modal/showcheckin.js
@@ -0,0 +1,3 @@
+//หน้าจอเช็คชื่อ
+//เป็น modal เอาไปใส่ใน classroom mange compoent
+//ลิ้งกับปุ่ม เพื่มการเช็คชื่อของ classroom mange
